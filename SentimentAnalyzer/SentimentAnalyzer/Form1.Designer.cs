3cf19fcae1b5c7b6b60a9494baec376ee052931f
Tilly Dewing <dewing.tilly@gmail.com>
2022-09-22 16:13:53 -0500

    Tokenizer & Placeholder UI
    Tokenizer for splitting reviews into a analyze able data structure.
    
    Place Holder UI for testing Purposes.

---
+++ SentimentAnalyzer/SentimentAnalyzer/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace SentimentAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReviewInputBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AnalyzeButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ResultsText = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ReviewInputBox
+            // 
+            this.ReviewInputBox.Location = new System.Drawing.Point(42, 56);
+            this.ReviewInputBox.Multiline = true;
+            this.ReviewInputBox.Name = "ReviewInputBox";
+            this.ReviewInputBox.Size = new System.Drawing.Size(360, 287);
+            this.ReviewInputBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter a Review:";
+            // 
+            // AnalyzeButton
+            // 
+            this.AnalyzeButton.Location = new System.Drawing.Point(427, 193);
+            this.AnalyzeButton.Name = "AnalyzeButton";
+            this.AnalyzeButton.Size = new System.Drawing.Size(75, 23);
+            this.AnalyzeButton.TabIndex = 2;
+            this.AnalyzeButton.Text = "Analyze";
+            this.AnalyzeButton.UseVisualStyleBackColor = true;
+            this.AnalyzeButton.Click += new System.EventHandler(this.AnalyzeButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ResultsText);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(629, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 287);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Results";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Sentiment:";
+            // 
+            // ResultsText
+            // 
+            this.ResultsText.AutoSize = true;
+            this.ResultsText.Location = new System.Drawing.Point(81, 25);
+            this.ResultsText.Name = "ResultsText";
+            this.ResultsText.Size = new System.Drawing.Size(0, 13);
+            this.ResultsText.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 375);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.AnalyzeButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ReviewInputBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ReviewInputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AnalyzeButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ResultsText;
+    }
+}
+
---
+++ SentimentAnalyzer/SentimentAnalyzer/Sentiment.cs
@@ -0,0 +1,87 @@
+﻿//Tilly Dewing COSC 4319 Spring 2022
+//Based of research and lexicon by Hu and Liu, KDD-2004, https://www.cs.uic.edu/~liub/FBS/sentiment-analysis.html
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalyzer
+{
+    class Sentiment //Used to try and determine the sentiment of a piece of english text
+    {
+        public static char[] delemiterCharsSents = {'.','?','!'}; //Used to split senteces (Ending puntuation)
+        public static char[] delemiterCharsWords = { ' ', ','};
+
+        public static int Analyze(string text, out float negPerc, out float neuPerc, out float posPerc) //Entry point for sentiment class
+        {
+            Tokenize(text);
+            negPerc = 0;
+            posPerc = 0;
+            neuPerc = 0;
+            return 0; //Returns -1 negative, 0 netural, 1 positive 
+        }
+        public static int Analyze(string text) //Calls analyze without additonal info
+        {
+            float X, Y, Z = 0;
+            return Analyze(text, out X, out Y, out Z);
+        }
+
+        //Idea is to break the sentence up into analyzable tagged chunks
+        static Paragraph Tokenize(string Text) //Overkill for now but Paragraph data structure will be nessesary for further work
+        {                               
+            Paragraph paragraph = new Paragraph();
+            paragraph.sentences = new List<Sentence>();
+            Console.WriteLine("Pre Tokenize:");
+            Console.WriteLine(Text);
+            Console.WriteLine("Tokenize:");
+
+            foreach (string sent in Text.Split(delemiterCharsSents)) //Split sentences
+            {
+                Console.WriteLine("Sent:" + sent);
+                Sentence sentence = new Sentence();
+                sentence.words = new List<TaggedWord>();
+
+                foreach (string word in sent.Split(delemiterCharsWords))//Split words
+                {
+                    if(word.Length > 0)
+                    {
+                        Console.WriteLine("TaggedWord: " + word);
+                        TaggedWord aWord;
+                        aWord.word = word;
+                        aWord.tag = Tag.ignore; //Temporary will use lexicon to determine Tag
+
+                        sentence.words.Add(aWord);
+                        paragraph.wordCount++;
+                    }
+                }
+                paragraph.sentences.Add(sentence);
+            }
+
+            return paragraph;
+        }
+
+    }
+    //Sentiment Data Structures
+    //Extra int fields are to allow detailed analysis later on.
+    enum Tag //Used to tag words inside of a sentence
+    { 
+        ignore, posWord, negWord, target, negation
+    }
+    struct TaggedWord
+    {
+        public string word;
+        public Tag tag;
+    }
+    struct Sentence
+    {
+        public List<TaggedWord> words;
+    }
+    struct Paragraph
+    {
+        public List<Sentence> sentences;
+        public int wordCount;
+        public int posWords;
+        public int negWords;
+    }
+}
