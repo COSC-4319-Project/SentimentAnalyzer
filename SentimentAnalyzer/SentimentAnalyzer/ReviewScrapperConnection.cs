65dc619fb900ee9e2e842cb5e22f70272465d66f
Tilly Dewing <dewing.tilly@gmail.com>
2022-10-20 16:29:18 -0500

    Began Scrapper connections

---
+++ SentimentAnalyzer/SentimentAnalyzer/ReviewScrapperConnection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+//Using Newtonsoft Json.NET for json parsing
+
+namespace SentimentAnalyzer
+{
+    class ReviewScrapperConnection
+    {
+        public static List<Review> reviews;
+        public static void LoadReviews(string jsonFileLoc)
+        {
+            string json = File.ReadAllText(jsonFileLoc);
+            reviews = JsonConvert.DeserializeObject<List<Review>>(json);
+        }
+    }
+
+    class Review
+    {
+        public string title;
+        public string body;
+        public byte rating;
+
+        public void ParseRating(string rating)
+        {
+            byte.TryParse(rating.Split(' ')[0], out this.rating);
+        }
+    }
+}
